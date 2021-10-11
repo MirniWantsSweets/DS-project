266c3ab5dfb6754e4dd96f23b2036b3353f8a9c7
1382324 <65294056+1382324@users.noreply.github.com>
2021-10-11 18:12:08 +0200

    Update Task.java

---
+++ src/Task.java
@@ -0,0 +1,23 @@
+public class Task extends Node implements Observador{
+int taskType; //Tipo de tarea (0 = corta, 1 = larga, 2 = rutinaria, ...)
+double totalTime;
+int ticks;
+
+  public Task(int tT) {
+    taskType = tT;
+    ticks = 0;
+    totalTime = 0.0;
+  }
+
+
+public double calculateTotalTime()
+{
+  double tickConstant  = 0.2; // == tiempo que representa cada tick en segundos.
+  return (ticks * tickConstant);
+}
+
+  @Override
+  public void update(){ // El observado notifica al observable, y se llama a esta función
+    ticks++;
+  }
+}
