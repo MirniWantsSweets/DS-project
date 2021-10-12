d665b0de5c0297f9dd27828da8b6814b8c7bc08b
1382324 <65294056+1382324@users.noreply.github.com>
2021-10-12 20:25:41 +0200

    TaskChanged

--- src/Task.java
+++ src/Task.java
@@ -2,18 +2,32 @@ public class Task extends Node implements Observer{
 int taskType; //Tipo de tarea (0 = corta, 1 = larga, 2 = rutinaria, ...)
 double totalTime;
 int ticks;
+boolean terminada; //Cuando terminada = True, tarea terminada.
 
-  public Task(int tT) {
+
+
+
+
+  public Task(int tT, String n) {
     taskType = tT;
+    nombre = nom
     ticks = 0;
     totalTime = 0.0;
+    terminada = false;
   }
 
 
-public double calculateTotalTime()
-{
+public double calculateTotalTime(){
   double tickConstant  = 0.2; // == tiempo que representa cada tick en segundos.
   return (ticks * tickConstant);
+}
+
+private void terminar(){
+terminada = true;
+}
+
+private canviNom(String nouNom){ //función para poder cambiar el nombre en cualquier momento.
+nom = nouNom;
 }
 
   @Override
