68924e64dffe9a41827e2871b8d8ead0636c77be
1382324 <65294056+1382324@users.noreply.github.com>
2021-10-26 23:10:58 +0200

    Observable comentado

--- src/Observable.java
+++ src/Observable.java
@@ -3,19 +3,19 @@ import java.util.List;
 import java.util.Iterator;
 
 public class Observable{
-  List<Observer> Observers= new ArrayList();
-  public void addObserver (Observer observer){
+  List<Observer> Observers= new ArrayList(); //Lista de observadores que serán notificados por el Observable
+  public void addObserver (Observer observer){ //Añadir observador a la lista de observadores
 
     Observers.add(observer);
 
   };
-  public void deleteObserver (Observer observer){
+  public void deleteObserver (Observer observer){ //Eliminar observador de la lista de observadores
 
     Observers.remove(Observers.indexOf(Observers));
 
   };
-  public void setChanged(){};
-  public void notifyObservers(){
+  public void setChanged(){}; //**//
+  public void notifyObservers(){ // Notifica a TODOS los observadores con el nuevo estado del observable
 
 
     for (int i=0;i<Observers.size(); i+=1){
